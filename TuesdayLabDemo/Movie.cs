dffebfdf35a68f36b87a3e61b76c08053fcddd99
Seth Bowman <brandonsethbowman@gmail.com>
2021-12-28 19:00:30 -0600

    Demo completed

---
+++ TuesdayLabDemo/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuesdayLabDemo
+{
+    public class Movie
+    {
+        public Movie()
+        {
+
+        }
+
+        public Movie(string title, string rating, int length, bool isGood)
+        {
+            Title = title;
+            Rating = rating;
+            Length = length;
+            IsGood = isGood;
+        }
+
+        public string Title { get; set; }
+        public string Rating { get; set; }
+        public int Length { get; set; }
+        public bool IsGood { get; set; }
+    }
+}
---
+++ TuesdayLabDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TuesdayLabDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var r = new Random();
+
+            //var numbers = new List<int>();
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    numbers.Add(r.Next(50));
+            //}
+
+            //var otherNums = numbers.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+            //otherNums.ForEach(x => Console.WriteLine(x));
+
+
+            // dot notation
+            var batman = new Movie();
+
+            batman.Title = "Batman";
+            batman.Length = 200;
+            batman.Rating = "PG13";
+            batman.IsGood = true;
+
+            // object initializer syntax
+
+            var returnOfTheKing = new Movie() 
+            {
+                Title = "Return Of The King",
+                Length = 300, 
+                Rating = "PG13",
+                IsGood = true 
+            };
+
+            // using a custom constructor
+
+            var bladeRunner = new Movie("Blade Runner", "R", 220, false);
+
+
+            var movies = new List<Movie>() 
+            { 
+                batman, 
+                returnOfTheKing, 
+                bladeRunner 
+            };
+
+
+            foreach (var item in movies)
+            {
+                Console.WriteLine($"This movie is called {item.Title}. It's length is {item.Length} minutes long. It is rated {item.Rating}. And it is {item.IsGood} that people like this movie.");
+            }
+
+
+        }
+    }
+}
